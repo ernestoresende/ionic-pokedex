438c1dc62005cbe2c33647482669e7cd5bc8026a
Ernesto Resende <ernestoresende@hotmail.com>
2020-08-24 09:25:04 -0300

    added: pokedex service handler

--- src/app/services/pokemon.service.ts
+++ src/app/services/pokemon.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators'
 
 @Injectable({
   providedIn: 'root'
 })
 export class PokemonService {
 
-  constructor() { }
+  // Base URL's for the Pokedex API
+  baseUrl = 'https://pokeapi.co/api/v2'
+  imageUrl = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon'
+
+  // Inject the HTTP client to make API calls
+  constructor(private http: HttpClient) {
+  }
+
+  getPokemon (offset = 0) {
+    return this.http.get(`${this.baseUrl}/pokemon?offset=${offset}&limit=25`).pipe(
+      map(result => {
+        return result['results']
+      })
+    )
+  }
 }
