638ba998cc94972dcec753afd55ae435a6fbbec3
Ernesto Resende <ernestoresende@hotmail.com>
2020-08-24 09:48:29 -0300

    added home page with pokemon list

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PokemonService } from '../services/pokemon.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  offset = 0
+  pokemon = []
 
-  constructor() {}
+  // Injects the pokemon.service as a private variable
+  constructor(private pokeService: PokemonService) {}
 
+  // Calls for the loadPokemon function OnInit
+  ngOnInit() {
+    this.loadPokemon()
+  }
+
+  // Gets Pokemon data from the API and logs it to the console
+  loadPokemon() {
+    this.pokeService.getPokemon(this.offset).subscribe(res => {
+      console.log('result: ', res)
+      this.pokemon = res
+    })
+  }
 }
